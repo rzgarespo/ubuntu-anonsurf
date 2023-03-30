1b5ccc36a74c7177bee88f24c2637feae287bff5
Rzgar <rzgarespo@gmail.com>
2023-03-30 07:29:43 +0200

    Updated installer.sh
    Updated installer script to be compatible with Ubuntu by removing Kali-specific checks and adding support for installing Tor on Ubuntu.

--- installer.sh
+++ installer.sh
@@ -2,9 +2,19 @@
 
 # Ensure we are being ran as root
 if [ $(id -u) -ne 0 ]; then
-	echo "This script must be ran as root"
-	exit 1
+    zenity --error --text "This script must be run as root."
+    exit 1
 fi
+
+# Check if tor is installed
+if ! which tor > /dev/null; then
+    # Install tor if it's not installed
+    apt-get update
+    apt-get install -y tor
+fi
+
+# Show a message box while the script runs
+zenity --info --text "This script will install Anonsurf on your system.\nPlease click 'OK' to continue." --title "Anonsurf Installer"
 
 # For upgrades and sanity check, remove any existing i2p.list file
 rm -f /etc/apt/sources.list.d/i2p.list
@@ -16,17 +26,13 @@ apt-key add /tmp/i2p-archive-keyring.gpg # Import the key
 rm /tmp/i2p-archive-keyring.gpg # delete the temp key
 apt-get update # Update repos
 
-if [[ -n $(cat /etc/os-release |grep kali) ]]
-then
-	apt-get install libservlet3.1-java libecj-java libgetopt-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni libjetty8-java #installs i2p and other dependencies
-	apt-get -f install # resolves anything else in a broken state
-fi
-
-apt-get install -y i2p-keyring #this will ensure you get updates to the repository's GPG key
-apt-get install -y secure-delete tor i2p # install dependencies, just in case
+apt-get install -y secure-delete i2p # install dependencies, just in case
 
 # Configure and install the .deb
 dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb # Build the deb package
 dpkg -i kali-anonsurf.deb || (apt-get -f install && dpkg -i kali-anonsurf.deb) # this will automatically install the required packages
+
+# Show a message box to indicate the installation is complete
+zenity --info --text "Anonsurf has been installed on your system." --title "Anonsurf Installer"
 
 exit 0
